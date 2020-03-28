c186499dd1eb12c65090877829ec11ba18f7859b
twopheat <twopheat@gmail.com>
2020-03-28 13:05:11 -0700

    first decent commit, square 1

---
+++ public/assets/js/script.js
@@ -0,0 +1,10 @@
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.carousel').carousel();
+    $('.dropdown-trigger').dropdown();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+  });
