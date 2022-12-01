7f9e4a51e9b05c4e8e3e5547405690bc71863e1e
twopheat <twopheat@gmail.com>
2022-12-01 15:56:34 -0800

    fixed google doc url

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -22,7 +22,7 @@ function Contact() {
                 
                 <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/twopheat/"><img className="social-icons text-center mt-4" alt="linkedin" src={linkedin} width="64" height="64"></img></a>
                 
-                <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1tM2gArCqJwi4Bgz1r7Zv7RR7vbGMIEYr/view?usp=sharing " title="RESUME"><img className="social-icons text-center mt-4" alt="resume" src={drive} width="64" height="64"></img></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/e/2PACX-1vRmccUE5nw4drSTCETK32gMlLtsO5Ahc2aCT4Hvwq7k0a6nIjrBJf0IdlvU57He6s8mmyaA6LZaffvY/pub" title="RESUME"><img className="social-icons text-center mt-4" alt="resume" src={drive} width="64" height="64"></img></a>
               
               </social>
         
